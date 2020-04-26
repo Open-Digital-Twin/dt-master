c60eb10bb3ce3f185a432eaecb8c49ca3bea1a08
Francisco Knebel <franciscopaivaknebel@gmail.com>
2020-04-26 13:16:00 -0300

    App state in scopes.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use actix_web::{App, HttpServer};
 
 mod routes;
-use routes::{index, index2};
+use routes::routes_config;
+
+struct AppState {
+  app_name: String,
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
   HttpServer::new(|| {
     App::new()
-      .service(index)
-      .service(index2)
+      .data(AppState {
+        app_name: String::from("Digital Twin")
+      })
+      .configure(routes_config)
   })
   .bind("127.0.0.1:8088")?
   .run()
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,8 +1,27 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{web, get, HttpResponse, Responder};
 
-pub mod auth;
+// pub mod auth;
+use super::AppState as AppState;
 
-#[get("/")]
+pub fn routes_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+      web::scope("/")
+        .data(AppState {
+          app_name: String::from("Test")
+        })
+        .service(index)
+        .service(index2)
+        .service(app)
+    );
+
+    /*
+    cfg.service(
+      web::scope("/auth")
+    )
+    */
+}
+
+#[get("")]
 pub async fn index() -> impl Responder {
   HttpResponse::Ok().body("Hello world!")
 }
@@ -11,5 +30,14 @@ pub async fn index() -> impl Responder {
 pub async fn index2() -> impl Responder {
   HttpResponse::Ok().body("Hello world again!")
 }
+
+#[get("/app")]
+async fn app(data: web::Data<AppState>) -> String {
+  let app_name = &data.app_name; 
+
+  format!("Hello {}!", app_name)
+}
+
+
 
 
