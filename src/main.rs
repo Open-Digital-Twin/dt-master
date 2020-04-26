a185611bd4efcb8f81e52979eb1b4c0b8816e303
Francisco Knebel <franciscopaivaknebel@gmail.com>
2020-04-26 21:58:12 +0000

    Add base auth web server.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
-use actix_web::{App, HttpServer};
+#[macro_use]
+extern crate bson;
+
+use actix_web::http::ContentEncoding;
+use actix_web::{middleware, web, App, HttpServer};
 
 mod routes;
 use routes::routes_config;
 
-struct AppState {
-  app_name: String,
-}
+mod config;
+mod db;
+mod middlewares;
+mod models;
+mod repositories;
+
+
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
   HttpServer::new(|| {
     App::new()
-      .data(AppState {
-        app_name: String::from("Digital Twin")
-      })
+      .wrap(middleware::Compress::new(ContentEncoding::Br))
+      .wrap(middleware::Logger::default())
+      .service(web::scope("/user").configure(repositories::user_repository::init_routes))
       .configure(routes_config)
   })
   .bind("0.0.0.0:3000")?
